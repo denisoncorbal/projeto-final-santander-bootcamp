8fde8f8b5686e8619bab43e2a4c82fd311ee3568
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2023-10-09 15:30:22 -0300

    feat(back): base project from start.spring.io

---
+++ backend/src/test/java/org/dgc/expensecontrol/ExpensecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dgc.expensecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
