2095ca8af3824740d88d0fd4a82c87216f2d33fb
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2023-10-10 16:43:20 -0300

    feat(front): basic componentization and style

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionHistoryComponent } from './pages/transaction-history/transaction-history.component';
+import { IncomeHistoryComponent } from './pages/income-history/income-history.component';
+import { OutcomeHistoryComponent } from './pages/outcome-history/outcome-history.component';
+import { AddIncomeComponent } from './pages/add-income/add-income.component';
+import { AddOutcomeComponent } from './pages/add-outcome/add-outcome.component';
 
 const routes: Routes = [
-  {path: '', component: undefined}
+  {path: '', redirectTo: "/transaction-history", pathMatch: 'full'},
+  {path: 'transaction-history', component: TransactionHistoryComponent},
+  {path: 'income-history', component: IncomeHistoryComponent},
+  {path: 'outcome-history', component: OutcomeHistoryComponent},
+  {path: 'add-income', component: AddIncomeComponent},
+  {path: 'add-outcome', component: AddOutcomeComponent},
 ];
 
 @NgModule({  
