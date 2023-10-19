3c870bf68d863a981fd4ac11a2d04d5b0fe71b9c
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2023-10-19 08:42:11 -0300

    fix: attempt to fix mix content back4app

--- frontend/src/environments/environment.back4app.ts
+++ frontend/src/environments/environment.back4app.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    webProtocol: "http://",
+    webProtocol: "https://",
     backendBasePath: "expensecontrol-omptr9n0.b4a.run",
     backendPort: "8080",
     backendVersion: "v1"
