c9d5ce1e34b793a7cc25aca09865d41e6c5c238a
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2023-11-04 09:28:04 -0300

    fix(back): fix token validation

--- backend/src/main/java/org/dgc/expensecontrol/security/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/dgc/expensecontrol/security/jwt/JwtAuthenticationFilter.java
@@ -41,11 +41,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       return;
     }
     final String authHeader = request.getHeader("Authorization");
-    System.out.println(authHeader);
     final String jwt;    
     final String userEmail;
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      System.out.println("### sem token ###");
       filterChain.doFilter(request, response);
       return;
     }
@@ -55,24 +53,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       userEmail = jwtService.extractUsername(jwt);
       System.out.println(userEmail);
     } catch (InvalidJwtException e) {
-      System.out.println("não extraiu email");
       throw new ServletException(e.getMessage());
     }
     if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-      System.out.println("tentar autenticar");
       UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
       var isTokenValid = tokenRepository.findByToken(jwt)
           .map(t -> !t.isExpired() && !t.isRevoked())
           .orElse(false);
       boolean isValid;
-      System.out.println("Token não expirado: " + isTokenValid);
       try {
         isValid = jwtService.isTokenValid(jwt, userDetails);
       } catch (InvalidJwtException e) {
-        // TODO Auto-generated catch block
         throw new ServletException(e.getMessage());
       }
-      System.out.println("Token não invalido: " + isValid);
       if (isValid && isTokenValid) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
             userDetails,
@@ -81,7 +74,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         authToken.setDetails(
             new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authToken);
-        System.out.println("Autenticou");
       }
     }
     filterChain.doFilter(request, response);
--- backend/src/main/java/org/dgc/expensecontrol/security/jwt/JwtService.java
+++ backend/src/main/java/org/dgc/expensecontrol/security/jwt/JwtService.java
@@ -3,6 +3,7 @@ package org.dgc.expensecontrol.security.jwt;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 
 import org.dgc.expensecontrol.model.RegisterUser;
@@ -15,7 +16,9 @@ import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
 import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.AudValidator;
 import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.IssValidator;
 import org.jose4j.jwt.consumer.JwtConsumer;
 import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.jose4j.jwt.consumer.JwtContext;
@@ -28,7 +31,7 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private final long ACCESS_TOKEN_EXPIRATION = 5;
-    private final long REFRESH_TOKEN_EXPIRATION = 5;
+    private final long REFRESH_TOKEN_EXPIRATION = 30;
     private final long NOT_VALID_YET = 0;
     private final String ISSUER = "ExpenseControlBackend";
     private final String AUDIENCE = "ExpenseControlFrontend";
@@ -52,23 +55,17 @@ public class JwtService {
     }
 
     public String extractUsername(String token) throws InvalidJwtException {
-        // return extractClaim(token, Claims::getSubject);
-        // TODO
 
         return extractClaim(token, t -> {
             try {
                 return t.getSubject();
             } catch (MalformedClaimException e) {
-                // TODO Auto-generated catch block
                 return null;
             }
         });
     }
 
     public <T> T extractClaim(String token, Function<JwtClaims, T> claimsResolver) throws InvalidJwtException {
-        // final Claims claims = extractAllClaims(token);
-        // return claimsResolver.apply(claims);
-        // TODO
         final JwtClaims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
@@ -96,15 +93,6 @@ public class JwtService {
             Map<String, Object> extraClaims,
             UserDetails userDetails,
             long expiration) throws JoseException {
-        // return Jwts
-        // .builder()
-        // .setClaims(extraClaims)
-        // .setSubject(userDetails.getUsername())
-        // .setIssuedAt(new Date(System.currentTimeMillis()))
-        // .setExpiration(new Date(System.currentTimeMillis() + expiration))
-        // .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-        // .compact();
-        // TODO
         JwtClaims claims = new JwtClaims();
         claims.setIssuer(ISSUER); // who creates the token and signs it
         claims.setAudience(AUDIENCE); // to whom the token is intended to be sent
@@ -115,30 +103,17 @@ public class JwtService {
                                                             // ago)
         claims.setSubject(userDetails.getUsername()); // the subject/principal is whom the token is about
         claims.setStringListClaim("extra", extraClaims.values().stream().map(String::valueOf).toList());
-        // A JWT is a JWS and/or a JWE with JSON claims as the payload.
-        // In this example it is a JWS so we create a JsonWebSignature object.
+
         JsonWebSignature jws = new JsonWebSignature();
 
-        // The payload of the JWS is JSON content of the JWT Claims
         jws.setPayload(claims.toJson());
 
-        // The JWT is signed using the private key
         jws.setKey(ELLIPTIC_CURVE_JSON_WEB_KEY.getPrivateKey());
 
-        // Set the Key ID (kid) header because it's just the polite thing to do.
-        // We only have one key in this example but a using a Key ID helps
-        // facilitate a smooth key rollover process
         jws.setKeyIdHeaderValue(ELLIPTIC_CURVE_JSON_WEB_KEY.getKeyId());
 
-        // Set the signature algorithm on the JWT/JWS that will integrity protect the
-        // claims
         jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P521_CURVE_AND_SHA512);
 
-        // Sign the JWS and produce the compact serialization or the complete JWT/JWS
-        // representation, which is a string consisting of three dot ('.') separated
-        // base64url-encoded parts in the form Header.Payload.Signature
-        // If you wanted to encrypt it, you can simply set this jwt as the payload
-        // of a JsonWebEncryption object and set the cty (Content Type) header to "jwt".
         String token = jws.getCompactSerialization();
         
         return token;
@@ -146,24 +121,18 @@ public class JwtService {
 
     public boolean isTokenValid(String token, UserDetails userDetails) throws InvalidJwtException {
         final String username = extractUsername(token);
-        System.out.println("Username == UserDetails.username: " + username.equals(userDetails.getUsername()));
-        System.out.println("Is token expired? " + isTokenExpired(token));
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) throws InvalidJwtException {
-        System.out.println("Expiration time from token: " + extractExpiration(token));
         return extractExpiration(token).before(new Date());
     }
 
     private Date extractExpiration(String token) throws InvalidJwtException {
-        // return extractClaim(token, Claims::getExpiration);
-        // TODO
         return new Date(extractClaim(token, t -> {
             try {
                 return t.getExpirationTime();
             } catch (MalformedClaimException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
                 return null;
             }
@@ -171,22 +140,14 @@ public class JwtService {
     }
 
     private JwtClaims extractAllClaims(String token) throws InvalidJwtException {
-        // return Jwts
-        // .parserBuilder()
-        // .setSigningKey(getSignInKey())
-        // .build()
-        // .parseClaimsJws(token)
-        // .getBody();
-        // TODO
         JwtConsumer jwtConsumer = new JwtConsumerBuilder()
-                .setSkipAllValidators()
-                .setDisableRequireSignature()
-                .setSkipSignatureVerification()
+                .setSkipAllDefaultValidators()
+                .registerValidator(new AudValidator(Set.of(AUDIENCE), true))
+                .registerValidator(new IssValidator(ISSUER, true))
+                .setVerificationKey(ELLIPTIC_CURVE_JSON_WEB_KEY.getPublicKey())
                 .build();
 
         JwtContext jwtContext = jwtConsumer.process(token);
-        JwtClaims res = jwtContext.getJwtClaims();
-        System.out.println(res);
-        return res;
+        return jwtContext.getJwtClaims();
     }
 }
